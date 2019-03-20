7cc77f2dc9fef7ee72a2f0e0cff97919d2afab51
Victoria Noci <vi.noci18@hotmail.com>
2019-03-20 18:38:56 +0100

    Bloques separados para eventos

--- blockly/blocks/handlers.js
+++ blockly/blocks/handlers.js
@@ -1,28 +1,4 @@
-Blockly.Blocks['_packet_in_handler'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("PACKET IN");
-    this.appendDummyInput()
-        .appendField("if MAC_dst")
-        .appendField(new Blockly.FieldDropdown([["==","=="], ["!=","!="]]), "conditions")
-        .appendField(new Blockly.FieldTextInput("'Escribe la MAC'"), "Mac-escrita");
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("--------------------------------------------------");
-    this.appendDummyInput()
-        .appendField("Port_Out")
-        .appendField(new Blockly.FieldCheckbox("TRUE"), "any_flood")
-        .appendField(new Blockly.FieldDropdown([["ANY","ANY"], ["FLOOD","FLOOD"]]), "port")
-        .appendField("or ")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "write_port")
-        .appendField(new Blockly.FieldTextInput("'Escribe el puerto'"), "port-escrito");
-    this.setColour(330);
- this.setTooltip("Simple handler for packet in event");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['_packet_in_handler2'] = {
+Blockly.Blocks['packet_in'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("PACKET IN");
@@ -30,23 +6,92 @@ Blockly.Blocks['_packet_in_handler2'] = {
         .appendField("if MAC_dst")
         .appendField(new Blockly.FieldDropdown([["==","=="], ["!=","!="]]), "conditions")
         .appendField(new Blockly.FieldTextInput("'Escribe la MAC'"), "mac-escrita");
+    this.appendStatementInput("events")
+        .setCheck("handler");
+    this.setNextStatement(true, "PortStatus");
+    this.setColour(345);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['packet_out'] = {
+  init: function() {
     this.appendDummyInput()
         .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("--------------------------------------------------");
+        .appendField("PACKET_OUT ");
     this.appendDummyInput()
-        .appendField(new Blockly.FieldColour("#33ff33"), "out-port")
-        .appendField("PACKETOUT ")
+        .appendField("Switch:")
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "switch");
+    this.appendDummyInput()
         .appendField("Out_Port:")
         .appendField(new Blockly.FieldCheckbox("TRUE"), "checkOP")
         .appendField(new Blockly.FieldDropdown([["ANY","ANY"], ["FLOOD","FLOOD"]]), "port")
         .appendField("or")
         .appendField(new Blockly.FieldCheckbox("FALSE"), "checkOP2")
-        .appendField(new Blockly.FieldTextInput("'Escribe el puerto'"), "port-out");
+        .appendField(new Blockly.FieldTextInput("'Puerto'"), "port-out");
+    this.setPreviousStatement(true, "handler");
+    this.setNextStatement(true, "handler");
+    this.setColour(180);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['flow_mod'] = {
+  init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldColour("#ff0000"), "flow-mod")
-        .appendField("FLOWMOD");
-    this.setColour(210);
- this.setTooltip("Packet In handler with OutPort and FlowMod Actions");
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField("FLOW_MOD");
+    this.appendDummyInput()
+        .appendField("Switch:")
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "switch");
+    this.appendDummyInput()
+        .appendField("Out_Port:")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "checkOP")
+        .appendField(new Blockly.FieldDropdown([["ANY","ANY"], ["FLOOD","FLOOD"]]), "port")
+        .appendField("or")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "checkOP2")
+        .appendField(new Blockly.FieldTextInput("'Puerto'"), "port-out");
+    this.setPreviousStatement(true, "handler");
+    this.setColour(60);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['packet_in2'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("PACKET IN");
+    this.appendDummyInput()
+        .appendField("if MAC_dst")
+        .appendField(new Blockly.FieldDropdown([["==","=="], ["!=","!="]]), "conditions")
+        .appendField(new Blockly.FieldTextInput("'Escribe la MAC'"), "mac-escrita");
+    this.appendStatementInput("packetout")
+        .setCheck("handler");
+    this.appendStatementInput("flowmod")
+        .setCheck("handler");
+    this.setColour(330);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['port_status'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("PORT_STATUS");
+    this.appendDummyInput()
+        .appendField("Switch:")
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "switch");
+    this.appendDummyInput()
+        .appendField("Port:")
+        .appendField(new Blockly.FieldTextInput("'Escribe el puerto'"), "port-out");
+    this.setPreviousStatement(true, "PortStatus");
+    this.setColour(120);
+ this.setTooltip("");
  this.setHelpUrl("");
   }
 };
--- blockly/generators/python/handlers.js
+++ blockly/generators/python/handlers.js
@@ -1,101 +1,48 @@
-Blockly.Python['_packet_in_handler'] = function(block) {
+Blockly.Python['packet_in'] = function(block) {
   var dropdown_conditions = block.getFieldValue('conditions');
-  var text_mac_escrita = block.getFieldValue('Mac-escrita');
-  var checkbox_any_flood = block.getFieldValue('any_flood') == 'TRUE';
-  var dropdown_port = block.getFieldValue('port');
-  var checkbox_write_port = block.getFieldValue('write_port') == 'TRUE';
-  var text_port_escrito = block.getFieldValue('port-escrito');
-  
-  // TODO: Assemble JavaScript into code variable.
-  var code = '    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)\n' +
-             '    def _packet_in_handler(self, ev):\n'+
-             '        msg = ev.msg\n' +
-             '        datapath = msg.datapath \n' +
-             '        ofproto = datapath.ofproto\n' +
-             '        dpid=datapath.id\n'+
-             '    # ANALIZAR EL PAQUETE\n' +
-             '        pkt = packet.Packet(msg.data)\n'+
-             '        eth_pkt = pkt.get_protocol(ethernet.ethernet)\n'+
-             '        dst = eth_pkt.dst\n\n'+
-             '        if dst '+ dropdown_conditions +' \'' + text_mac_escrita + '\':\n'+
-             '            # COMPROBACION DE LOS DOS CHECKS\n'+
-	     '            if \'' + checkbox_any_flood + '\' == \'true\' and \'' + checkbox_write_port + '\' == \'true\':\n'+
-             '                print \'Solo puedes seleccionar una condicion para el puerto\'\n'+
-             '                print \'----------------------------------\'\n'+
-             '            # DESPLEGABLE (any o flood)\n'+
-	     '            elif \'' + checkbox_any_flood + '\' == \'true\':\n'+
-             '                out_port = ofproto.OFPP_' + dropdown_port + '\n'+
-             '                print \'Puerto salida \'+ str(out_port) + \'' + dropdown_port + '\'\n'+ 
-             '            # PUERTO ESCRITO A MANO\n'+
-             '            elif \'' + checkbox_write_port + '\' == \'true\':\n'+
-             '                out_port = ' + text_port_escrito + '\n'+
-             '                print \'Puerto salida \'+ str(out_port)\n'+
-             '            print \'Identificador switch \' + str(dpid)\n'+
-             '            print \'MAC destino \' str(dst)\n'+
-             '            print \'----------------------------------\'\n'+
-             '        else:\n'+
-             '            print \'Descartamos el paquete porque no cumple la condicion\'\n'+
-             '            print \'----------------------------------\'\n';
+  var text_mac_escrita = block.getFieldValue('mac-escrita');
+  var statements_events = Blockly.Python.statementToCode(block, 'events');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
   return code;
 };
 
-
-Blockly.Python['_packet_in_handler2'] = function(block) {
-  var dropdown_conditions = block.getFieldValue('conditions');
-  var text_mac_escrita = block.getFieldValue('mac-escrita');
-  var colour_out_port = block.getFieldValue('out-port');
+Blockly.Python['packet_out'] = function(block) {
+  var dropdown_switch = block.getFieldValue('switch');
   var checkbox_checkop = block.getFieldValue('checkOP') == 'TRUE';
   var dropdown_port = block.getFieldValue('port');
   var checkbox_checkop2 = block.getFieldValue('checkOP2') == 'TRUE';
   var text_port_out = block.getFieldValue('port-out');
-  var colour_flow_mod = block.getFieldValue('flow-mod');
-  
   // TODO: Assemble Python into code variable.
-  var code = '    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)\n' +
-             '    def _packet_in_handler(self, ev):\n'+
-             '        msg = ev.msg\n' +
-             '        datapath = msg.datapath \n' +
-             '        ofproto = datapath.ofproto\n' +
-             '        parser = datapath.ofproto_parser\n'+
-             '        dpid=datapath.id\n'+
-             '        # self.mac_to_port.setdefault(dpid, {})\n'+
-             '    # ANALIZAR EL PAQUETE\n' +
-             '        pkt = packet.Packet(msg.data)\n'+
-             '        eth_pkt = pkt.get_protocol(ethernet.ethernet)\n'+
-             '        in_port = msg.match[\'in_port\']\n'+
-             '        src = eth_pkt.src\n'+
-             '        dst = eth_pkt.dst\n\n'+
-             '        if dst '+ dropdown_conditions +' \'' + text_mac_escrita + '\':\n'+
-             '            # COLOR DE PACKET_OUT\n'+
-             '            if \'' + colour_out_port + '\' == \'#33ff33\':\n'+
-             '                # COMPROBACION DE LOS DOS CHECKS\n'+
-	     '                if \'' + checkbox_checkop + '\' == \'true\' and \'' + checkbox_checkop2 + '\' == \'true\':\n'+
-             '                    print \'Solo puedes seleccionar una condicion para el puerto\'\n'+
-             '                    print \'----------------------------------\'\n'+
-             '                # DESPLEGABLE (any o flood)\n'+
-	     '                elif \'' + checkbox_checkop + '\' == \'true\':\n'+
-             '                    out_port = ofproto.OFPP_' + dropdown_port + '\n'+
-             '                    print \'Puerto salida \'+ str(out_port) + \'' + dropdown_port + '\'\n'+ 
-             '                # PUERTO ESCRITO A MANO\n'+
-             '                elif \'' + checkbox_checkop2 + '\' == \'true\':\n'+
-             '                    out_port = ' + text_port_out + '\n'+
-             '                    print \'Puerto salida \'+ str(out_port)\n'+
-             '                print \'Identificador switch \' + str(dpid)\n'+
-             '                print \'MAC destino \' str(dst)\n'+
-             '                print \'----------------------------------\'\n\n'+
-             '            # COLOR DE FLOW_MOD\n'+
-             '            if \'' + colour_flow_mod + '\' == \'#33ff33\':\n'+
-             '                #Aprender MAC para evitar FLOOD la proxima vez\n'+
-             '                self.mac_to_port[dpid][src] = in_port\n'+
-             '                #Construir action list\n'+
-             '                actions = [parser.OFPActionOutput(out_port)]\n'+
-             '                #Instalar regla para evitar packet_in la proxima vez\n'+
-             '                if out_port != ofproto.OFPP_FLOOD:\n'+
-             '                    match = parser.OFPMatch(in_port=in_port, eth_dst=dst)\n'+
-             '                    self.add_flow(datapath,1,match,actions)\n'+
-             '        else:\n'+
-             '            print \'Descartamos el paquete porque no cumple la condicion\'\n'+
-             '            print \'----------------------------------\'\n';           
+  var code = '...\n';
   return code;
 };
 
+Blockly.Python['flow_mod'] = function(block) {
+  var dropdown_switch = block.getFieldValue('switch');
+  var checkbox_checkop = block.getFieldValue('checkOP') == 'TRUE';
+  var dropdown_port = block.getFieldValue('port');
+  var checkbox_checkop2 = block.getFieldValue('checkOP2') == 'TRUE';
+  var text_port_out = block.getFieldValue('port-out');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['packet_in2'] = function(block) {
+  var dropdown_conditions = block.getFieldValue('conditions');
+  var text_mac_escrita = block.getFieldValue('mac-escrita');
+  var statements_packetout = Blockly.Python.statementToCode(block, 'packetout');
+  var statements_flowmod = Blockly.Python.statementToCode(block, 'flowmod');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['port_status'] = function(block) {
+  var dropdown_switch = block.getFieldValue('switch');
+  var text_port_out = block.getFieldValue('port-out');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
